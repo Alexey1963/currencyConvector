1b64c155b5706c2edd60960ed3d51f7eac32165e
Shumakher <gzullm02041963@googlemail.com>
2021-12-28 11:56:03 +0300

    second release

--- src/script.js
+++ src/script.js
@@ -11,14 +11,12 @@ function init() {
     const blocks = [];
 
     function inProgress() {
-        shadow.classList.toggle('off');
-        shadow.classList.toggle('on');
-        changeBtn.classList.toggle('off');
-        changeBtn.classList.toggle('on');
+        shadow.classList.add('on');
+        changeBtn.classList.remove('on');
     }
 
     function inquire(inputId) {
-        console.log(blocks[0].value, blocks[1].value, inputId);
+        console.log(blocks[inputId - 1].value, blocks[inputId % 2].value, inputId);
 
         if (blocks[0].value !== blocks[1].value) {
             timeout = setTimeout(() => {
@@ -33,24 +31,25 @@ function init() {
         return Math.round(x * 10000) / 10000;
     }
 
-    function answer(rates, inputId) {
+    function answer(rates, id) {
+
         clearTimeout(timeout);
-        if(timeout === 0) {
-            // inProgress();
-        } 
-        
-        console.log(inputId)
+        shadow.classList.remove('on')
+        changeBtn.classList.add('on')
+        console.log(id)
 
         let factor = 0;
         for (let x of Object.values(rates)) {
             factor = x;
-            console.log(factor, blocks[0].inputField.value, blocks[1].inputField.value);
-            blocks[1].inputField.value = round(blocks[0].inputField.value * factor);
+
+            console.log(factor, blocks[id-1].inputField.value, blocks[id % 2].inputField.value);
             blocks[0].rateField.innerText = `1 ${blocks[0].value} = ${round(factor) + ' ' + blocks[1].value}`
-            if (factor < 1) {
-                factor = 1 / factor;
-                blocks[1].rateField.innerText = `1 ${blocks[1].value} = ${round(factor) + ' ' + blocks[0].value}`
+            blocks[1].rateField.innerText = `1 ${blocks[1].value} = ${round(1/factor) + ' ' + blocks[0].value}`
+
+            if(id === 2) {
+                factor = 1 / x;
             }
+            blocks[id % 2].inputField.value = round(blocks[id-1].inputField.value * factor);
         }
     }
 
@@ -58,19 +57,24 @@ function init() {
         const currencyInput = new CurrencyInput(index + 1, currencyArr, currency, inquire);
         blocks.push(currencyInput);
     });
-    inquire(0);
+    inquire(1);
 
     changeBtn.addEventListener('click', (e) => {
         console.log(e.currentTarget);
-        blocks.reverse();
-        blocks[0].inputField.value = blocks[1].inputField.value
-        changeBtn.parentElement.insertBefore(blocks[0].container, changeBtn);
-        let title = blocks[0].container.querySelector('.title');
-        title.innerText = 'У меня есть';
-        changeBtn.parentElement.append(blocks[1].container);
-        title = blocks[1].container.querySelector('.title');
-        title.innerText = 'Хочу приобрести';
-        inquire();
+        // blocks.reverse();
+        // blocks[0].inputField.value = blocks[1].inputField.value
+        // changeBtn.parentElement.insertBefore(blocks[0].container, changeBtn);
+        // let title = blocks[0].container.querySelector('.title');
+        // title.innerText = 'У меня есть';
+        // changeBtn.parentElement.append(blocks[1].container);
+        // title = blocks[1].container.querySelector('.title');
+        // title.innerText = 'Хочу приобрести';
+        let value1 = blocks[0].value;
+        let value2 = blocks[1].value;
+        blocks[0].setValue(value2);
+        blocks[1].setValue(value1);
+        console.log(blocks[0].value, blocks[1].value)
+        inquire(1);
     })
 
     console.log(blocks);
@@ -79,27 +83,27 @@ function init() {
 
 
 
-
 class CurrencyInput {
     constructor(inputId, currencyList, defaultValue, callback) {
         this.value = defaultValue;
         const block = document.querySelector(`#block-${inputId}`);
-        this.container = block;
         this.inputField = block.querySelector(`#input-${inputId}`);
         this.inputField.value = 1;
         this.rateField = block.querySelector('.rate');
         const select = block.querySelector('select');
         const btns = block.querySelectorAll('.btn:not(select)');
         this.menu = block.querySelector('.menu');
-        // callback(inputId);
-
+        this.container = block;
+        this.select = select;
+        this.btns = btns;
+        
         select.addEventListener('change', () => {
             block.querySelector('.selected').classList.remove('selected');
             select.classList.add('selected');
             this.value = select.value;
             callback(inputId);
         })
-
+        
         btns.forEach(btn => {
             btn.addEventListener('click', () => {
                 block.querySelector('.selected').classList.remove('selected');
@@ -108,30 +112,43 @@ class CurrencyInput {
                 callback(inputId);
             })
         })
-
+        
         currencyList.forEach((currencyText) => {
             const option = document.createElement('option');
             option.innerText = currencyText;
             select.append(option);
         });
-
+        
         const input = block.querySelector('input')
         input.addEventListener('change', (e) => {
             this.inputField.value = e.target.value;
-            console.log(this.inputField)
+            console.log(this.inputField, inputId)
             callback(inputId);
         })
-
+        
     }
-
+    
+    setValue(value) {
+        const btn = [...this.btns].find(btn => btn.innerText === value);
+        this.container.querySelector('.selected').classList.remove('selected');
+        if (btn) {
+            btn.classList.add('selected');
+        } else {
+            const options = this.select.querySelectorAll('option');
+            const option = [...options].find(option => option.value === value);
+            option.selected = true;
+            this.select.classList.add('selected');
+        }
+        this.value = value;
+    }
 }
 
 const API = {
     async request(base, symbols, inputId, callback) {
-        console.log(base, symbols, callback);
         try {
             res = await fetch(`https://api.exchangerate.host/latest?base=${base}&symbols=${symbols}`)
             data = await res.json();
+            console.log(data.rates)
             callback(data.rates, inputId);
         } catch (e) {
             console.log(e.message)
